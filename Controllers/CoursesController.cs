6e906568e5820dbf463b107371a53a0103242eaa
baoxommoi2 <baoxommoi2@gmail.com>
2021-07-07 12:09:11 +0700

    Add project files.

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,50 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            //get List category
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+
+            return View(objCourse);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            //ko xét valid LectureId
+            ModelState.Remove("LecturerId");
+            if (ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            //Lấy Login user id
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            //add vào csdl
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+
+            //Về home
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
